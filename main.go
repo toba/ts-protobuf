7046135b55630e7484af469c6ddaca22e6b4623a
Jason Abbott <dev@trailimage.com>
2017-10-12 10:57:13 -0600

    refactor as plugin

--- main.go
+++ main.go
@@ -53,13 +53,14 @@ import (
 	"os"
 
 	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/protoc-gen-go/generator"
 )
 
 func main() {
 	// Begin by allocating a generator. The request and response structures are stored there
 	// so we can do error handling easily - the response structure contains the field to
 	// report failure.
-	g := NewGenerator()
+	g := generator.New()
 
 	data, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
