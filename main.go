b593346535b532e4ec74e5825e0a228320d2264f
Jason Abbott <dev@trailimage.com>
2017-10-12 13:14:28 -0600

    revert to non-plugin

--- main.go
+++ main.go
@@ -53,14 +53,13 @@ import (
 	"os"
 
 	"github.com/golang/protobuf/proto"
-	"github.com/golang/protobuf/protoc-gen-go/generator"
 )
 
 func main() {
 	// Begin by allocating a generator. The request and response structures are stored there
 	// so we can do error handling easily - the response structure contains the field to
 	// report failure.
-	g := generator.New()
+	g := NewGenerator()
 
 	data, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
